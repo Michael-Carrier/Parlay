3a909ee87bb64d8f3cd54e8edf6bb44c123e2134
mcarrier <mcarrier@keywordsstudios.com>
2026-03-15 14:38:06 -0400

    forgot to add a json file3-pewdipie

--- en/convertsrt2json.py
+++ en/convertsrt2json.py
@@ -40,4 +40,4 @@ def convert_srt_to_json(input_file, output_file):
     print(f"Success! {len(json_data)} lines converted to {output_file}")
 
 # Usage
-convert_srt_to_json('I Had 3 DAYS To MASTER Fortnite Competitive.srt', 'I Had 3 DAYS To MASTER Fortnite Competitive_en.json')
+convert_srt_to_json('Pewdipie-90-Day-Fiance.srt', 'Pewdipie-90-Day-Fiance_en.json')
